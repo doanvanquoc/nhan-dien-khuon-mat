5631ffdf0b3d1d7918d3350516014961bfd69dd2
doanvanquoc <vanquoc.dev@gmail.com>
2024-07-12 15:02:15 +0700

    api nhận diện giọng nói

--- api/routes.py
+++ api/routes.py
@@ -2,12 +2,10 @@ from flask import Blueprint, request, jsonify
 from .embeddings import get_face_encoding
 from .faiss_index import FaissIndex
 import os
-import numpy as np
 import json
-import sounddevice as sd
-import wavio
 import speech_recognition as sr
 import re
+from datetime import datetime
 
 routes = Blueprint("routes", __name__)
 
@@ -15,20 +13,21 @@ d = 128
 faiss_index = FaissIndex(d)
 
 USER_DATA_DIR = "user_data"
+LOGIN_HISTORY_DIR = "login_history"
 if not os.path.exists(USER_DATA_DIR):
     os.makedirs(USER_DATA_DIR)
 
+if not os.path.exists(LOGIN_HISTORY_DIR):
+    os.makedirs(LOGIN_HISTORY_DIR)
 
 INDEX_MAPPING_PATH = "index_mapping.json"
 FAISS_INDEX_PATH = "faiss_index.bin"
-
 
 if os.path.exists(INDEX_MAPPING_PATH):
     with open(INDEX_MAPPING_PATH, "r") as file:
         index_mapping = json.load(file)
 else:
     index_mapping = {}
-
 
 number_map = {
     "một": "1",
@@ -85,36 +84,39 @@ def verify():
         return jsonify({"error": "No image uploaded"}), 400
 
     image_file = request.files["image"]
+    image_path = os.path.join(LOGIN_HISTORY_DIR, image_file.filename)
+    image_file.save(image_path)
 
-    temp_dir = "temp_images"
-    if not os.path.exists(temp_dir):
-        os.makedirs(temp_dir)
-    temp_image_path = os.path.join(temp_dir, image_file.filename)
-    image_file.save(temp_image_path)
-
-    face_encoding = get_face_encoding(temp_image_path)
-
-    os.remove(temp_image_path)
+    face_encoding = get_face_encoding(image_path)
 
     if face_encoding is None:
+        os.remove(image_path)
         return jsonify({"error": "No face detected"}), 400
 
     D, I = faiss_index.search_embedding(face_encoding)
     print(f"Khoảng cách giữa 2 vector: {D[0][0]}")
     if D[0][0] < 0.19:
-
         closest_index = I[0][0]
-
         original_filename = index_mapping.get(str(closest_index), None)
 
         if original_filename:
+            # Đổi tên file lưu với tên file + ngày giờ
+            timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+            new_image_path = os.path.join(
+                LOGIN_HISTORY_DIR,
+                f"{os.path.splitext(image_file.filename)[0]}_{timestamp}.jpg",
+            )
+            os.rename(image_path, new_image_path)
+
             return (
                 jsonify({"message": "User verified", "filename": original_filename}),
                 200,
             )
         else:
+            os.remove(image_path)
             return jsonify({"error": "Verification failed - original_filename"}), 400
     else:
+        os.remove(image_path)
         return jsonify({"error": "Verification failed - unverify"}), 400
 
 
@@ -156,4 +158,4 @@ def transcribe():
 
     os.remove(audio_path)
 
-    return (jsonify({"text": final_text}), 200)
+    return jsonify({"text": final_text}), 200
